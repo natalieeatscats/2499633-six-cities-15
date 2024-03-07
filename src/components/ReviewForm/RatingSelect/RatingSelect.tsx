8f3dfb5dd203675158b2cc462d3b86e9dc59372f
Natalia Popova <93385001+natalieeatscats@users.noreply.github.com>
2024-03-07 16:55:28 +0300

    Update RatingSelect.tsx

--- src/components/ReviewForm/RatingSelect/RatingSelect.tsx
+++ src/components/ReviewForm/RatingSelect/RatingSelect.tsx
@@ -3,23 +3,21 @@ import { RatingStar } from './RatingStar/RatingStar';
 type TProps = {
   onRatingChange: (rating: number) => void;
 }
+const ratingValues = [
+  5,
+  4,
+  3,
+  2,
+  1
+];
 
-export const RatingSelect = ({ onRatingChange }: TProps) => {
-  const ratingValues = [
-    5,
-    4,
-    3,
-    2,
-    1
-  ];
-  return (
-    <>
-      <label className="reviews__label form__label" htmlFor="review">
+export const RatingSelect = ({ onRatingChange }: TProps) => (
+  <>
+    <label className="reviews__label form__label" htmlFor="review">
       Your review
-      </label>
-      <div className="reviews__rating-form form__rating">
-        {ratingValues.map((value) => (<RatingStar value={value} onChange={onRatingChange} key={`rating-${value}`}></RatingStar>))}
-      </div>
-    </>
-  );
-};
+    </label>
+    <div className="reviews__rating-form form__rating">
+      {ratingValues.map((value) => (<RatingStar value={value} onChange={onRatingChange} key={`rating-${value}`}></RatingStar>))}
+    </div>
+  </>
+);
